c9ea79230513d343c34e9715cee0285700af7fe1
Alex Romney <alexromney1215@gmail.com>
2016-12-16 21:49:46 -0700

    added some component files

---
+++ client/components/Events.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Events;
---
+++ client/components/UserEvent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserEvent extends Component {
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default UserEvent;
