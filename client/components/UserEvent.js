0bcee7ab833ac44376b4168684aeca7727dc073a
Spencer Richards <spencer.richards7@gmail.com>
2016-12-21 23:52:54 -0700

    event updated

--- client/components/UserEvent.js
+++ client/components/UserEvent.js
@@ -3,29 +3,41 @@ import React, { Component } from 'react';
 class UserEvent extends Component {
 
   render() {
+    let sportEvent = this.props.userEvent;
     return(
-      <div>
-
-        <div className="col s12 m7">
-          <h2 className="header">Horizontal Card</h2>
-          <div className="card horizontal">
-            <div className="card-image">
-              <img src="http://lorempixel.com/100/190/nature/6" />
-            </div>
-            <div className="card-stacked">
-              <div className="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
+      <div className='container'>
+        <div>
+          <div className="col s12 m12">
+            <h5 className="header">{ sportEvent.name }</h5>
+            <div className="card event-card horizontal">
+              <div className="card-image">
+                <img src="http://lorempixel.com/100/190/nature/6" />
               </div>
-              <div className="card-action">
-                <a href="#">This is a link</a>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <div className='row'>
+                    <div className='col m4'>
+                      <i>{ sportEvent.sport }</i>
+                    </div>  
+                    <div className='col m4'>
+                      <i>{ sportEvent.skill_level }</i>
+                    </div>
+                    <div className='col m4'>
+                      <i>0.2 miles </i>
+                    </div>  
+                      <div>
+                      </div> 
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
       </div>
     );
   }
 }
+
+
 
 export default UserEvent;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,32 @@
 10.times do 
-	User.create(username: Faker::Internet.user_name,
-							first_name: Faker::Name.first_name,
-							last_name: Faker::Name.last_name,
-							dob: Faker::Date.between(1000.days.ago, Date.today),
-							phone_number: Faker::PhoneNumber.cell_phone,
-							favorite: ['basketball', 'soccer', 'football', 'golf'].sample,
-							email: Faker::Internet.email,
-							password: 'password')
-
+	user = User.create(username: Faker::Internet.user_name,
+										first_name: Faker::Name.first_name,
+										last_name: Faker::Name.last_name,
+										dob: Faker::Date.between(1000.days.ago, Date.today),
+										phone_number: Faker::PhoneNumber.cell_phone,
+										favorite: ['basketball', 'soccer', 'football', 'golf'].sample,
+										email: Faker::Internet.email,
+										password: 'password')
 end
 
+	# 3.times do 
+	# 	user.events.create(name: Faker::App.name,
+	# 													sport: Faker::Team.sport,
+	# 													date: Faker::Date.forward(23),
+	# 													time: [1..12].sample,
+	# 													capacity: Faker::Number.between(2, 30),
+	# 													venue: Faker::Book.title,
+	# 													street: Faker::Address.street_name,
+	# 													city: Faker::Address.city,
+	# 													state: Faker::Address.state,
+	# 													zip: Faker::Address.zip,
+	# 													skill_level: ['Open', 'AA', 'A', 'B', 'Novice', 'Everyone'].sample,
+	# 													description: Faker::Lorem.paragraph)
+	# end
+
+
+
+
+
 puts 'Users Seeded!'
+puts 'Events Seeded!'
