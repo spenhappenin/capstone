ce8d8e29518ab55e82f05e1d5f4c1d7cc505ab46
Spencer Richards <spencer.richards7@gmail.com>
2016-12-28 16:22:31 -0700

    fixed merge conflicts

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import About from './About';
 import Picture from './Picture';
 import Contact from './Contact';
+import NavBar from './NavBar';
 
 class Landing extends Component {
 
   render() {
     return(
-      <div>
-        <Picture />
-        <About />
-        <Contact />
-
-      </div>
+			<div className="box">
+			  <div className="rows header">
+			  	<NavBar />
+			  </div>
+			  <div className="rows content">
+			    <Picture />
+			  </div>
+			    <About />
+        	<Contact />
+			</div>
     );
   }
 }
 
 export default Landing;
+
+
+
+
+       
