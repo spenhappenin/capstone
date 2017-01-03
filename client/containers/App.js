953c373a43f2b2be47d8905002a3717f0480d188
Spencer Richards <spencer.richards7@gmail.com>
2017-01-03 16:32:08 -0700

    fixed chinese map issue

--- client/containers/App.js
+++ client/containers/App.js
@@ -58,7 +58,8 @@ class App extends React.Component {
       <div>
         <nav className='nav-bar'>
           <div className='nav-wrapper'>
-          <Link to='/' className='main-logo'> <img className='responsive-img' src='logo.jpg' /> </Link>
+          <Link to='/' className='main-logo'> <img className='responsive-img' src='logo1.png' height='60px' width='60px' />GO</Link>
+
             <a href='#' data-activates='mobile' className='button-collapse'>
               <i className='fa fa-bars'></i>
             </a>
