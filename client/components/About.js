daa13b76fe2f84c2a1c2e83843ce90a51ce701a5
Spencer Richards <spencer.richards7@gmail.com>
2017-01-05 20:44:37 -0700

    spencers social added to about page

--- client/components/About.js
+++ client/components/About.js
@@ -11,6 +11,11 @@ class About extends Component {
               <div className='spencer z-depth-5 responsive-img'> </div>
               <hr />
               <p className='center'><em> Ginger </em></p>
+              <div className='social-media'>
+                <a href='https://github.com/spenhappenin' target='_blank' rel='noopener'><img className='social-icons responsive-img' style={{height: '55px', width: '55px'}} src='spencer_github.png' /></a>
+                <a href='https://www.linkedin.com/in/spencer-richards' target='_blank' rel='noopener'><img className='social-icons responsive-img' style={{height: '40px', width: '40px', marginTop: '6px'}} src='http://res.cloudinary.com/omash612/image/upload/v1483478100/linkedinlogo_sjckc6.png' /> </a>
+                <a href='spencer.richards7@gmail.com'><img className='social-icons responsive-img' src="http://res.cloudinary.com/omash612/image/upload/v1483478773/gmailLogo_as7oja.png" style={{ height: '40px', width: '40px', marginTop: '6px'}}></img></a>
+              </div>
             </div>
             <div className='col s3'>
               <div className='alex z-depth-5 responsive-img'> </div>
