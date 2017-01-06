960a30056220cb3aa20a76da4dc4e9531c31333d
rebeccashifflett <no.choshoku@gmail.com>
2017-01-06 16:06:00 -0700

    disables game button when capacity is met

--- client/components/Counter.js
+++ client/components/Counter.js
@@ -5,7 +5,10 @@ class Counter extends Component {
     super(props);
     this.state = { count: 1 };
     this.incrementCount = this.incrementCount.bind(this);
+    this.gameOpen = this.gameOpen.bind(this);
+    this.gameClosed = this.gameClosed.bind(this);
 }
+
   incrementCount(){
     if (this.state.count === this.props.capacity) {
     } else {
@@ -14,21 +17,38 @@ class Counter extends Component {
       });
     }
  }
-
-
-
-render () {
-  return (
- <div className='row'>
-   <div className='box'>
-     <div className='col s12'>
-      <label htmlFor='capacity'>Attending</label>
-      <h1>{this.state.count}</h1>
-      <button className="btn" onClick={this.incrementCount}>Join</button>
+ gameOpen() {
+   return(
+   <div className='row'>
+      <div className='box'>
+        <div className='col s12'>
+          <label htmlFor='capacity'>Attending</label>
+          <h1>{this.state.count}</h1>
+          <button className="btn" onClick={this.incrementCount}>Join</button>
+        </div>
+      </div>
     </div>
- </div>
- </div>
-)};
-}
+)}
 
+  gameClosed() {
+    return(
+    <div className='row'>
+      <div className='box'>
+        <div className='col s12'>
+         <label htmlFor='capacity'>Full</label>
+         <h1>{this.state.count}</h1>
+         <button className="btn-flat disabled">Full</button>
+       </div>
+    </div>
+    </div>
+  )}
+
+render() {
+  if (this.state.count != this.props.capacity) {
+    return(this.gameOpen());
+  }else{
+    return(this.gameClosed());
+  }
+}
+}
 export default Counter;
