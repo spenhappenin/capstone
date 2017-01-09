2e245dbfa6de20a8c6ab7049ace0a9af75b0e34a
rebeccashifflett <no.choshoku@gmail.com>
2017-01-09 11:57:57 -0700

    about me blurbs

--- client/components/About.js
+++ client/components/About.js
@@ -37,26 +37,25 @@ class About extends Component {
             <div className='col s3'>
               <div className='william z-depth-5'> </div>
               <hr />
-              <p className='center'><em> Huge Beard, Tatted Dog Lover </em></p>
+              <p className='center' style={{fontSize: '12px'}}><span style={{fontWeight: 'bolder', fontSize: '16px', color: '#027e99'}}> William Winger</span> is a Utah native and animal rescue advocate.  He studied web development at DevPoint Labs and his professional experience spans from manufacturing manager to electrician. His passion for coding led him to a full stack course in Ruby and JavaScript. When he is not creating web applications he enjoys spending time with his Labrador retriever, Maddie. </p>
               <div className='social-media'>
                <a href='https://github.com/WilliamW101' target='_blank'><img className='social-icons' style={{height: '55px', width: '55px'}} src='http://res.cloudinary.com/omash612/image/upload/v1483734378/william_github_olct83.png' /></a>
                <a href='https://www.linkedin.com/in/william-winger' target='_blank'><img className='social-icons' style={{height: '40px', width: '40px', marginTop: '6px'}} src='http://res.cloudinary.com/omash612/image/upload/v1483478100/linkedinlogo_sjckc6.png' /> </a>
                <a href='mailto:billy_thekid55@hotmail.com'><img className='social-icons' src="http://res.cloudinary.com/omash612/image/upload/v1483478773/gmailLogo_as7oja.png" style={{ height: '40px', width: '40px', marginTop: '6px'}}></img></a>
              </div>
             </div>
+
             <div className='col s3'>
               <div className='rebecca z-depth-5'> </div>
               <hr />
-              <p className='center'><em> Yoga Master Extraordinaire </em></p>
-              <div className='social-media'>
+              <p className='center' style={{fontSize: '12px'}}><span style={{fontWeight: 'bolder', fontSize: '16px', color: '#027e99'}}> Rebecca Shifflett</span> holds a bachelor’s and master’s in Public Health and is a certified yoga instructor. She believes staying active and connected to others is vital for personal wellness and growth. She recently completed a full stack web development course at DevPoint Labs and is eager to continue a career path in web development.  </p>
                 <a href='https://github.com/rebeccashifflett' target='_blank' rel='noopener'><img className='social-icons responsive-img' style={{height: '55px', width: '55px'}} src='http://res.cloudinary.com/omash612/image/upload/v1483734375/femalecodertocat_dimxz8.png' /></a>
                 <a href='www.linkedin.com/in/rebecca-shifflett-rs' target='_blank' rel='noopener'><img className='social-icons responsive-img' style={{height: '40px', width: '40px', marginTop: '6px'}} src='http://res.cloudinary.com/omash612/image/upload/v1483478100/linkedinlogo_sjckc6.png' /> </a>
                 <a href='mailto:no.choshoku@gmail.com'><img className='social-icons responsive-img' src="http://res.cloudinary.com/omash612/image/upload/v1483478773/gmailLogo_as7oja.png" style={{ height: '40px', width: '40px', marginTop: '6px'}}></img></a>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+</div>
     );
   }
 }
