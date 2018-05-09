bc59d2311dbb5d303e24208e8f48ec851e8b5312
William Winger <Billy_thekid55@hotmail.com>
2018-05-08 22:48:36 -0600

    Create Event works and creates an event \o/

--- automation/login.rb
+++ automation/login.rb
@@ -6,8 +6,9 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 
 website = "http://localhost:3000/"
-eventLocation = "USANA"
+eventLocation = "Magna Rec Center"
 do_it = "Do it! Just do it! Don't let your dreams be dreams. Yesterday, you said tomorrow. So just do it! Make your dreams come true! Just do it! Some people dream of success, while you're gonna wake up and work hard at it! Nothing is impossible! You should get to the point where anyone else would quit, and you're not gonna stop there! No, what are you waiting for? Do it! Just do it! Yes you can! Just do it. If you're tired of starting over, stop giving up!"
+event_name_array = ['Fist Fight Hockey', 'Badmouth Badmiton', 'Flippin Frisbee', 'Champion Ping Pong', 'Tennis Talk Up', 'Voyager Volleyball']
 
 # ===============================
 # |         Go To Site          |
@@ -111,7 +112,7 @@ create_event.click
 
 # Fill in info for creating an event
 create_event_name = driver.find_element(:id, 'createEventName')
-create_event_name.send_keys 'Rec Basketball'
+create_event_name.send_keys event_name_array.sample
 
 # Select sport from dropdown
 choose_sport_dropdown = driver.find_element(:class, 'select-dropdown')
@@ -127,19 +128,15 @@ today_date.click
 close_picker = driver.find_element(:class, 'picker__close')
 close_picker.click
 
-# Select a time for the event (not working)
-event_time = driver.find_element(:xpath, "//input[@type='time']")
-sleep 2
-event_time.send_keys(:arrow_up)
-sleep 2
-event_time.send_keys(:tab)
-event_time.send_keys(:arrow_up)
-event_time.send_keys(:tab)
-event_time.send_keys(:arrow_up)
+# Select a time for the event (Super Jenky)
+# event_time = driver.find_element(:xpath, "//input[@type='time']")
+# event_time.send_keys :arrow_up, :tab, :arrow_down, :tab, :arrow_up
+event_time = driver.find_element(:xpath, "//input[@type='number']")
+event_time.send_keys [:shift, :tab], [:shift, :tab], [:shift, :tab], :arrow_up, :tab, :arrow_up, :tab, :arrow_down
 
-# Set number of people to attend
+# Set number of people to attend (Kinda Jenky)
 event_capacity = driver.find_element(:xpath, "//input[@type='number']")
-event_capacity.send_keys "10"
+event_capacity.send_keys :backspace, "10"
 
 # Event Venue
 event_location = driver.find_element(:xpath, "//input[@placeholder='Dimple Dell Recreation']")
@@ -147,11 +144,11 @@ event_location.send_keys(eventLocation)
 
 # Event Address
 event_address = driver.find_element(:xpath, "//input[@placeholder='1234 USA Drive']")
-event_address.send_keys 'Electric Ave'
+event_address.send_keys '3270 S 8400 W'
 
 # Event City
 event_city = driver.find_element(:xpath, "//input[@placeholder='Salt Lake City']")
-event_city.send_keys 'Magna'
+event_city.send_keys 'West Valley'
 
 # Event State
 event_state = driver.find_element(:xpath, "//label[text()=' State ']/following::input[@class='select-dropdown']")
@@ -174,8 +171,10 @@ select_skill.click
 
 # Event Description
 event_blurb = driver.find_element(:xpath, "//textarea[@placeholder='Write Description Here...']")
-event_blurb.send_keys(do_it)
+event_blurb.send_keys(do_it, :arrow_up, :arrow_up)
 
 # Create Event
-create_event = driver.find_element(:class, 'blue')
+create_event = driver.find_element(:css, "input[class='center btn blue']")
 create_event.click
+
+sleep 2
